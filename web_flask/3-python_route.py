1a678dfa2ee1a53d93ebb590521afd2d2d987f5f
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-03-21 16:59:10 +0200

    task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """It a script that starts a Flask web application"""
-#!/usr/bin/python3
-""" Script that starts a Flask web application """
 from flask import Flask
 
 app = Flask(__name__)
@@ -10,27 +8,26 @@ app.url_map.strict_slashes = False
 
 @app.route('/')
 def hello_hbnb():
-    """ Print Web """
+    """ Print the Web """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
 def hbnb():
-    """ Print Web """
+    """ Print the Web """
     return 'HBNB'
 
 
 @app.route('/c/<text>')
 def c_is_fun(text):
-    """ Print a char C followed by the value of the text variable """
+    """ Print char C followed by value of a text variable """
     return 'C {}'.format(text.replace('_', ' '))
 
 
 @app.route('/python')
 @app.route('/python/<text>')
 def python_is_cool(text='is cool'):
-    """ Print Python, followed by the value of the text variable,
-    with default value of text: is cool"""
+    """ Print Python, followed by value of a text variable"""
     return 'Python {}'.format(text.replace('_', ' '))
 
 
