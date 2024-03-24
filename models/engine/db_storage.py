01dc97eeeee6102af37a1e1068b21b96a473da9c
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-03-24 16:32:29 +0200

    task 7 try

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,6 +74,5 @@ class DBStorage:
         self.__session = Session()
 
     def close(self):
-        """ calls remove()
-        """
+        """Closes the session."""
         self.__session.close()
